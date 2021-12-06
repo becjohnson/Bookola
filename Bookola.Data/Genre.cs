f33c8506bb68ff6f08f08cecf8d3525f541d2a82
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-12-06 09:26:06 -0500

    Initial Commit

---
+++ Bookola.Data/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookola.Data
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
