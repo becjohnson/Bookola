006c1ee133f84a23d1223836fc5785e1ce79f2c9
Marjorie Sandoval <Marjorie.Sandoval74@gmail.com>
2021-12-06 10:41:41 -0500

    Magazine Data added

---
+++ Bookola.Data/Magazine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookola.Data
+{
+    public class Magazine
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public Guid UserId { get; set; }
+        public int Countrycode { get; set; }
+        public int ReadingLevel { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+        
+        [Required]
+        public int Volume { get; set; }
+        
+        [Required]
+        public DateTime IssueDate { get; set; }
+        
+        [Required]
+        public int ISSN { get; set; }
+        
+        [ForeignKey("GenreId")]
+        public Genre Genre { get; set; }
+    }
+}
