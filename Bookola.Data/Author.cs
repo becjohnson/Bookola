ca44f695a156635d18dbd7f919af83acfffdc210
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-12-07 12:17:41 -0500

    Initial

---
+++ Bookola.Data/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookola.Data
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        [Display(Name = "Author")]
+        public string FullName 
+        {
+            get => LastName + ", " + FirstName;
+            set { }
+        }
+        public string FirstName { get; set; }
+        [Key]
+        public string LastName { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
